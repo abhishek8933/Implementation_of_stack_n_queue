09c0e19e2594041d784f8ed71d8ab2612c08c558
ABHISHEK <abhishekmaurya5667@gmail.com>
2023-03-25 20:45:59 +0530

    commit changes

--- queue.js
+++ queue.js
@@ -1,23 +1,28 @@
 //implementation of queue using single Array
 class Queue{
+    //initialising our queue
     constructor(){
         this.array=[]
     }
+    //pushing an element inside our queue
     push_element(element){
         this.array.push(element);
     }
+    //deleting an element inside our queue as it follows FIFO rule
     dequeue(){
         for(let i=0;i<this.array.length-1;i++){
             this.array[i]=this.array[i+1];
         }
         this.array.pop();
     }
+    //printing all the elements inside our queue
     print_queue(){
         console.log('printing the elements inside the queue');
         for(let i=0;i<this.array.length;i++){
             console.log(this.array[i]);
         }
     }
+    //printing the first element inside our queue
     first_element_inside_queue(){
         if(this.array.length===0){
             console.log('Queue is Empty');
@@ -25,12 +30,14 @@ class Queue{
             console.log(this.array[0]);
         }
     }
+    //checking whether our queue is empty or not
     is_Empty(){
         return this.array.length===0;
     }
 }
-
+//creating an instance of queue
 let queue=new Queue();
+//performing all the operations
 queue.push_element(10);
 queue.push_element(20);
 queue.push_element(30);
--- stack.js
+++ stack.js
@@ -2,12 +2,15 @@
 //implementation of stack using arrays
 
 class Stack{
+    //initialising our array 
     constructor(){
         this.st=[];
     }
+    //pushing elements into stack
     push_elements(element){
         this.st.push(element);
     }
+    //pop of element inside our stack as it uses LIFO rule
     pop_element(){
         if(this.st.length===0){
             return "Stack is empty";
@@ -17,17 +20,20 @@ class Stack{
         return element;
         }
     }
+    //check our stack is empty or not
     is_empty(){
         return this.st.length===0;
     }
+    //printing all the elements which is inside our stack
     print_stack_element(){
         for(let i=0;i<this.st.length;i++){
             console.log(this.st[i]);
         }
     }
 }
+//creating an instance of class Stack
 let stack=new Stack();
-
+//performing operations
 stack.push_elements(10);
 stack.push_elements(20);
 stack.push_elements(30);
